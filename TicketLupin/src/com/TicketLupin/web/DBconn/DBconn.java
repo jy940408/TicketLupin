d24f01e01ff1585b1e1c9f2fa7b863fac81a4da1
jy940408 <junyoung940408@gmail.com>
2021-03-05 23:43:15 +0900

    20210305_commit_3

--- TicketLupin/src/com/TicketLupin/web/DBconn/DBconn.java
+++ TicketLupin/src/com/TicketLupin/web/DBconn/DBconn.java
@@ -6,25 +6,47 @@ import java.sql.DriverManager;
 public class DBconn {
 
 	
-	
+/*	
 	Connection conn =null;
-   //占쏙옙占쏙옙 占쏙옙占쏙옙
+	
    String coninfo ="jdbc:oracle:thin:@127.0.0.1:1521:xe";
    String idinfo ="TicketLupin";
    String pwdinfo ="1234";
    
    public Connection getConnection() {
       try {
-         //占쏙옙占쏙옙譴占쏙옙占쏙옙占� OrcleDriver占쏙옙 찾占승댐옙
          Class.forName("oracle.jdbc.driver.OracleDriver");
-         //占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占싼댐옙
+         System.out.println("after forName");
          conn= DriverManager.getConnection(coninfo, idinfo, pwdinfo);
-               
+         System.out.println("DBms connection success");
+		 System.out.println("DB load success");
          } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("DB load fail " + e.toString());
          }
          return conn;
    }
+  */
+	
+	Connection conn =null;
+	
+	   String coninfo ="jdbc:oracle:thin:@10.211.55.4:1521:xe";
+	   String idinfo ="TicketLupin";
+	   String pwdinfo ="1234";
+	   
+	   public Connection getConnection() {
+	      try {
+	         Class.forName("oracle.jdbc.driver.OracleDriver");
+	         System.out.println("after forName");
+	         conn= DriverManager.getConnection(coninfo, idinfo, pwdinfo);
+	         System.out.println("DBms connection success");
+			 System.out.println("DB load success");
+	         } catch (Exception e) {
+	            e.printStackTrace();
+	            System.out.println("DB load fail " + e.toString());
+	         }
+	         return conn;
+	   }
 	/*
 	
 	Connection conn = null;
